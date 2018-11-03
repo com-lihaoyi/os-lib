584d8bf5ff77be494f4acd24884fe8063669a785
Li Haoyi <haoyi.sg@gmail.com>
2018-11-03 10:36:52 +0800

    Give up trying to unit test setting groups/owners

--- os/test/src/test/os/FilesystemPermissionsTests.scala
+++ os/test/src/test/os/FilesystemPermissionsTests.scala
@@ -20,22 +20,26 @@ object FilesystemPermissionsTests extends TestSuite {
     }
     'owner - {
       * - prep { wd =>
-
-      }
-      'set - {
-        * - prep { wd =>
-
-        }
+        // Only works as root :(
+        //
+        // val originalOwner = os.owner(wd / "File.txt")
+        //
+        // os.owner.set(wd / "File.txt", "nobody")
+        // os.owner(wd / "File.txt").getName ==> "nobody"
+        //
+        // os.owner.set(wd / "File.txt", originalOwner)
       }
     }
     'group - {
       * - prep { wd =>
-
-      }
-      'set - {
-        * - prep { wd =>
-
-        }
+        // Only works as root :(
+        //
+        // val originalOwner = os.owner(wd / "File.txt")
+        //
+        // os.owner.set(wd / "File.txt", "nobody")
+        // os.owner(wd / "File.txt").getName ==> "nobody"
+        //
+        // os.owner.set(wd / "File.txt", originalOwner)
       }
     }
   }
