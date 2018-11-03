d34a8d2e63c354e5a3e5e4f4df354d867ece1676
Li Haoyi <haoyi.sg@gmail.com>
2018-11-03 10:49:08 +0800

    Flesh out SpawningSubprocessesTests

--- os/test/src/test/os/SpawningSubprocessesTests.scala
+++ os/test/src/test/os/SpawningSubprocessesTests.scala
@@ -1,5 +1,7 @@
 package test.os
 
+import java.io.{BufferedReader, InputStreamReader}
+
 import test.os.TestUtil.prep
 import utest._
 
@@ -8,17 +10,54 @@ object SpawningSubprocessesTests extends TestSuite {
     'proc - {
       'call - {
         * - prep { wd =>
+          val res = os.proc('ls, wd/"folder2").call()
 
+          res.exitCode ==> 0
+
+          res.out.string ==>
+            """nestedA
+              |nestedB
+              |""".stripMargin
+
+          res.out.trim ==>
+            """nestedA
+              |nestedB""".stripMargin
+
+          res.out.lines ==> Seq(
+            "nestedA",
+            "nestedB"
+          )
+
+          res.out.bytes
+
+
+          val fail = os.proc('ls, "doesnt-exist").call(cwd = wd)
+
+          fail.exitCode ==> 1
+
+          fail.out.string ==> ""
+
+          assert(fail.err.string.contains("No such file or directory"))
         }
       }
       'stream - {
         * - prep { wd =>
-
+          var lineCount = 1
+          os.proc('find, ".").stream(
+            cwd = wd,
+            onOut = (buf, len) => lineCount += buf.slice(0, len).count(_ == '\n'),
+            onErr = (buf, len) => () // do nothing
+          )
+          lineCount ==> 21
         }
       }
       'spawn - {
         * - prep { wd =>
-
+          val sub = os.proc("python", "-c", "print eval(raw_input())").spawn(cwd = wd)
+          val out = new BufferedReader(new InputStreamReader(sub.getInputStream))
+          sub.getOutputStream.write("1 + 2 + 3\n".getBytes)
+          sub.getOutputStream.flush()
+          out.readLine() ==> "6"
         }
       }
     }
