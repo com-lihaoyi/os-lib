fd98bfe288a4668db363e2f7de9c67f063f68d1f
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2023-11-23 16:37:57 +0100

    Fix nohometest on windows

--- os/nohometest/src/NoHomeTests.scala
+++ os/nohometest/src/NoHomeTests.scala
@@ -3,12 +3,21 @@ package test.os
 import utest._
 
 object NoHomeTests extends TestSuite {
+  private lazy val isWindows = sys.props("os.name").toLowerCase().contains("windows")
+
   val tests = Tests {
     test("pwd when home is not available") {
       System.setProperty("user.home", "?")
       val homeException = intercept[IllegalArgumentException] { os.home }
         .getMessage()
-      assert(homeException == "requirement failed: ? is not an absolute path")
+
+      val expectedException =
+        if (isWindows)
+          "Illegal char <?> at index 0: ?"
+        else
+          "requirement failed: ? is not an absolute path"
+
+      assert(homeException == expectedException)
       os.pwd
       ()
     }
