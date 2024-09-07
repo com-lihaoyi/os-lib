e2538e64f8faf2b598c887f364b325739e96e812
Li Haoyi <haoyi.sg@gmail.com>
2024-09-07 19:06:53 +0800

    .

--- os/src-jvm/package.scala
+++ os/src-jvm/package.scala
@@ -39,9 +39,19 @@ package object os {
   /**
    * The current working directory for this process.
    */
-  def pwd: Path = dynamicPwd.value
-  val dynamicPwd: DynamicVariable[Path] =
-    new DynamicVariable(os.Path(java.nio.file.Paths.get(".").toAbsolutePath))
+  def pwd: Path = dynamicPwdFunction.value()
+
+  private val pwd0 = os.Path(java.nio.file.Paths.get(".").toAbsolutePath)
+
+  /**
+   * Used to override `pwd` within a certain scope with a generated value
+   */
+  val dynamicPwdFunction: DynamicVariable[() => Path] = new DynamicVariable(() => dynamicPwd.value)
+
+  /**
+   * Used to override `pwd` within a certain scope with a fixed value
+   */
+  val dynamicPwd: DynamicVariable[Path] = new DynamicVariable(pwd0)
 
   val up: RelPath = RelPath.up
 
