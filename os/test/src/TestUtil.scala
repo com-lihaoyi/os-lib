697cd1a7fcce5d7a29b0dcbe53ce56807d8ef2bd
Li Haoyi <haoyi.sg@gmail.com>
2024-09-09 17:05:08 +0800

    .

--- os/test/src/TestUtil.scala
+++ os/test/src/TestUtil.scala
@@ -57,7 +57,7 @@ object TestUtil {
       }
     )
 
-    val original = Paths.get(sys.env("MILL_TEST_RESOURCE_FOLDER"), "test")
+    val original = Paths.get(sys.env("OS_TEST_RESOURCE_FOLDER"), "test")
     Files.walkFileTree(
       original,
       new SimpleFileVisitor[Path]() {
