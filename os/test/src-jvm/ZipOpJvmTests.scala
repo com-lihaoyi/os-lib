9fc2526d43b9f91896afd153fa13199ba6633cee
Li Haoyi <haoyi.sg@gmail.com>
2024-10-07 12:26:09 +0200

    .

--- os/test/src-jvm/ZipOpJvmTests.scala
+++ os/test/src-jvm/ZipOpJvmTests.scala
@@ -11,6 +11,46 @@ object ZipOpJvmTests extends TestSuite {
 
   def tests = Tests {
 
+    test("zipAndUnzipFolder") - prep { wd =>
+      // Zipping files and folders in a new zip file
+      val zipFileName = "zip-file-test.zip"
+      val zipFile1: os.Path = os.zip(
+        dest = wd / zipFileName,
+        sources = Seq(
+          wd / "File.txt",
+          wd / "folder1"
+        )
+      )
+      // Adding files and folders to an existing zip file
+      os.zip(
+        dest = zipFile1,
+        sources = Seq(
+          wd / "folder2",
+          wd / "Multi Line.txt"
+        )
+      )
+
+      // Unzip file to a destination folder
+      val unzippedFolder = os.unzip(
+        source = wd / zipFileName,
+        dest = wd / "unzipped folder"
+      )
+
+      val paths = os.walk(unzippedFolder)
+      val expected = Seq(
+        // Files get included in the zip root using their name
+        wd / "unzipped folder/File.txt",
+        wd / "unzipped folder/Multi Line.txt",
+        // Folder contents get included relative to the source root
+        wd / "unzipped folder/nestedA",
+        wd / "unzipped folder/nestedB",
+        wd / "unzipped folder/one.txt",
+        wd / "unzipped folder/nestedA/a.txt",
+        wd / "unzipped folder/nestedB/b.txt"
+      )
+      assert(paths.sorted == expected)
+    }
+    
     test("zipAndUnzipPreserveMtimes") - prep { wd =>
       // Create a file and set its modification time
       val testFile = wd / "FileWithMtime.txt"
@@ -96,6 +136,66 @@ object ZipOpJvmTests extends TestSuite {
     test("zipAndUnzipDontPreserveMtimes") {
       test("noAppend") - prep { wd => zipAndUnzipDontPreserveMtimes(wd, false) }
       test("append") - prep { wd => zipAndUnzipDontPreserveMtimes(wd, true) }
+    }
+
+    test("deletePatterns") - prep { wd =>
+      val amxFile = "File.amx"
+      os.copy(wd / "File.txt", wd / amxFile)
+
+      // Zipping files and folders in a new zip file
+      val zipFileName = "zipByDeletingCertainFiles.zip"
+      val zipFile1: os.Path = os.zip(
+        dest = wd / zipFileName,
+        sources = List(
+          wd / "File.txt",
+          wd / amxFile,
+          wd / "Multi Line.txt"
+        )
+      )
+
+      os.zip(
+        dest = zipFile1,
+        deletePatterns = List(amxFile.r)
+      )
+
+      // Unzip file to check for contents
+      val outputZipFilePath = os.unzip(
+        zipFile1,
+        dest = wd / "zipByDeletingCertainFiles"
+      )
+      val paths = os.walk(wd / "zipByDeletingCertainFiles").sorted
+      val expected = Seq(
+        outputZipFilePath / "File.txt",
+        outputZipFilePath / "Multi Line.txt"
+      )
+
+      assert(paths == expected)
+    }
+
+
+    test("open") - prep { wd =>
+      val zipFile = os.zip.open(wd / "zip-test.zip")
+      try {
+        os.copy(wd / "File.txt", zipFile / "File.txt")
+        os.copy(wd / "folder1", zipFile / "folder1")
+        os.copy(wd / "folder2", zipFile / "folder2")
+      } finally zipFile.close()
+
+      val zipFile2 = os.zip.open(wd / "zip-test.zip")
+      try {
+        os.list(zipFile2) ==> Vector(
+          zipFile2 / "File.txt",
+          zipFile2 / "folder1",
+          zipFile2 / "folder2"
+        )
+        os.remove.all(zipFile2 / "folder2")
+        os.remove(zipFile2 / "File.txt")
+      } finally zipFile2.close()
+
+      val zipFile3 = os.zip.open(wd / "zip-test.zip")
+      try os.list(zipFile3) ==> Vector(zipFile3 / "folder1")
+      finally zipFile3.close()
+
     }
   }
 }
