0a223e20cd9f14855b3babf7103161d3aeb540d6
Li Haoyi <haoyi.sg@gmail.com>
2024-10-07 12:17:41 +0200

    .

--- os/src-native/PlatformShims.scala
+++ os/src-native/PlatformShims.scala
@@ -5,5 +5,5 @@ import java.nio.file.attribute.PosixFilePermission
 private[os] object PlatformShims {
   // Scala-Native does not support reading posix file permissions
   // https://github.com/scala-native/scala-native/issues/4067
-  def readPermissions(file: java.io.File): Option[java.util.Set[PosixFilePermission]] = None
+  def readPermissions(file: java.nio.file.Path): Option[java.util.Set[PosixFilePermission]] = None
 }
