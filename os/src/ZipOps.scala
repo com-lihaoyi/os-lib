431e4c7f829f14ec9e19313e3d87b38be74d13e6
Kien Dang <kiend@proton.me>
2025-03-17 00:07:08 +0800

    Edit doc

--- os/src/ZipOps.scala
+++ os/src/ZipOps.scala
@@ -31,14 +31,14 @@ object zip {
   /**
    * Zips the provided list of files and directories into a single ZIP archive.
    *
-   * If `dest` already exists and is a zip, performs modifications to `dest` in place
-   * rather than creating a new zip.
+   * Unix file permissions will be preserved when creating a new zip, i.e. when `dest` does not already exists.
    *
-   * When adding new files to an existing zip,
-   * - UNIX file permissions will be preserved if Java Runtime Version >= 14
-   * - if using Java Runtime Version < 14, UNIX file permissions are not preserved, even for existing zip entries
+   * If `dest` already exists and is a zip, performs modifications to `dest` in place
+   * rather than creating a new zip. In that case,
+   * - Unix file permissions will be preserved if Java Runtime Version >= 14
+   * - if using Java Runtime Version < 14, Unix file permissions are not preserved, even for existing zip entries
    * - symbolics links will always be stored as the referenced files
-   * - existing symbolic links stored the zip might lose their symbolic link file type field
+   * - existing symbolic links stored in the zip might lose their symbolic link file type field and become broken
    *
    * @param dest      The path to the destination ZIP file.
    * @param sources      A list of paths to files and directories to be zipped. Defaults to an empty list.
@@ -47,7 +47,7 @@ object zip {
    * @param preserveMtimes Whether to preserve modification times (mtimes) of the files.
    * @param deletePatterns A list of regular expression patterns to delete files from an existing ZIP archive before appending new ones.
    * @param compressionLevel number from 0-9, where 0 is no compression and 9 is best compression. Defaults to -1 (default compression).
-   * @param followLinks Whether to store symbolic links as the referenced files. Default to true. Setting this to false has no effect when modifying a zip file in place.
+   * @param followLinks Whether to store symbolic links as the referenced files. Default to `true`. Setting this to `false` has no effect when modifying a zip file in place.
    * @return The path to the created ZIP archive.
    */
   def apply(
