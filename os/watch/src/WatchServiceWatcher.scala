5443f89370c5f2ee1c9200cf8989307006a1d7b5
Artūras Šlajus <x11@arturaz.net>
2025-05-07 14:09:13 +0300

    Improvements to `oslib.watch` (#386)
    - Remove extraneous `println`s in `WatchServiceWatcher`
    - Add `filter: os.Path => Boolean` parameter to `watch`
    - Correctly handle `OVERFLOW` events.
    - Bump scala versions to allow compilation on newer JDKs.

--- os/watch/src/WatchServiceWatcher.scala
+++ os/watch/src/WatchServiceWatcher.scala
@@ -14,9 +14,9 @@ import scala.util.Properties.isWin
 class WatchServiceWatcher(
     roots: Seq[os.Path],
     onEvent: Set[os.Path] => Unit,
-    logger: (String, Any) => Unit = (_, _) => ()
+    filter: os.Path => Boolean,
+    logger: (String, Any) => Unit
 ) extends Watcher {
-
   val nioWatchService = FileSystems.getDefault.newWatchService()
   val currentlyWatchedPaths = mutable.Map.empty[os.Path, WatchKey]
   val newlyWatchedPaths = mutable.Buffer.empty[os.Path]
@@ -46,7 +46,7 @@ class WatchServiceWatcher(
           modifiers: _*
         )
       )
-      newlyWatchedPaths.append(p)
+      if (filter(p)) newlyWatchedPaths.append(p)
     }
     bufferedEvents.add(p)
   }
@@ -61,12 +61,33 @@ class WatchServiceWatcher(
 
     logger("WATCH KINDS", events.map(_.kind()))
 
+    def logWarning(msg: String): Unit = {
+      System.err.println(s"[oslib.watch] (path=$p) $msg")
+    }
+
+    def logWarningContextNull(e: WatchEvent[_]): Unit = {
+      logWarning(
+        s"Context is null for event kind='${e.kind().name()}' of class ${e.kind().`type`().getName}, " +
+          s"this should never happen."
+      )
+    }
+
     for (e <- events) {
-      bufferedEvents.add(p / e.context().toString)
+      if (e.kind() == OVERFLOW) {
+        logWarning("Overflow detected, some filesystem changes may not be registered.")
+      } else {
+        contextSafe(e) match {
+          case Some(ctx) => bufferedEvents.add(p / ctx.toString)
+          case None => logWarningContextNull(e)
+        }
+      }
     }
 
     for (e <- events if e.kind() == ENTRY_CREATE) {
-      watchSinglePath(p / e.context().toString)
+      contextSafe(e) match {
+        case Some(ctx) => watchSinglePath(p / ctx.toString)
+        case None => logWarningContextNull(e)
+      }
     }
 
     watchKey.reset()
@@ -83,7 +104,8 @@ class WatchServiceWatcher(
         val listing =
           try os.list(top)
           catch {
-            case e: java.nio.file.NotDirectoryException => Nil
+            case _: java.nio.file.NotDirectoryException | _: java.nio.file.NoSuchFileException =>
+              Nil
           }
         for (p <- listing) watchSinglePath(p)
         bufferedEvents.add(top)
@@ -124,10 +146,10 @@ class WatchServiceWatcher(
 
       } catch {
         case e: InterruptedException =>
-          println("Interrupted, exiting: " + e)
+          logger("Interrupted, exiting.", e)
           isRunning.set(false)
         case e: ClosedWatchServiceException =>
-          println("Watcher closed, exiting: " + e)
+          logger("Watcher closed, exiting.", e)
           isRunning.set(false)
       }
   }
@@ -137,7 +159,7 @@ class WatchServiceWatcher(
       isRunning.set(false)
       nioWatchService.close()
     } catch {
-      case e: IOException => println("Error closing watcher: " + e)
+      case e: IOException => logger("Error closing watcher.", e)
     }
   }
 
@@ -146,4 +168,6 @@ class WatchServiceWatcher(
     onEvent(bufferedEvents.toSet)
     bufferedEvents.clear()
   }
+
+  def contextSafe[A](e: WatchEvent[A]): Option[A] = Option(e.context())
 }
