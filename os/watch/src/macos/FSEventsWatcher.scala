3eec633b91511342c5764807ce22c81c42d7043e
Artūras Šlajus <x11@arturaz.net>
2025-06-21 21:18:11 +0300

    Remove run-loop macOS API.

--- os/watch/src/macos/FSEventsWatcher.scala
+++ os/watch/src/macos/FSEventsWatcher.scala
@@ -17,7 +17,6 @@ class FSEventsWatcher(
 ) extends Watcher {
   import CoreServicesApi.INSTANCE._
   import DispatchApi.INSTANCE._
-  import FSEventsWatcher.kCFRunLoopDefaultMode
 
   @volatile private var closed = false
 
@@ -61,15 +60,10 @@ class FSEventsWatcher(
   /** Used to signal the watcher to stop. */
   private val signal: Object = new Object
 
-  private val useRunLoop: Boolean = true
-
-  @volatile private var currentRunLoop: Option[CFRunLoopRef] = None
-
   def run(): Unit = {
-    assert(!closed, "Cannot run a closed watcher.")
+    assert(!closed, "Cannot `run()` a closed watcher.")
 
     def startFsEventStream[A](streamRef: FSEventStreamRef)(f: => A): A = {
-      //              logger("FSEventsWatcher.run: starting stream", ())
       val success = FSEventStreamStart(streamRef)
       if (!success) throw new IllegalStateException("FSEventStreamStart returned false")
 
@@ -77,32 +71,15 @@ class FSEventsWatcher(
       finally FSEventStreamStop(streamRef)
     }
 
-    def waitUntilWeAreToldToStopViaSignal() = {
-      //                logger("FSEventsWatcher.run: waiting for stop signal", ())
-      signal.synchronized {
-        try {
-          signal.wait()
-          //                    logger("FSEventsWatcher.run: received stop signal, cleaning up.", ())
-        }
-        catch {
-          case _: InterruptedException =>
-          //                      logger("FSEventsWatcher.run: received interrupt, cleaning up.", ())
-        }
-      }
-    }
-
-//    logger("FSEventsWatcher.run: starting", ())
     val pathsToWatchCfStrings = NativeUtils.assertAllSome(
       srcs.iterator.map(path => CFStringRef(path.toString)).toArray,
       CFRelease
     ).getOrElse(throw new IllegalStateException(s"Can't create CFStrings for $srcs, some values were null"))
     try {
-//      logger("FSEventsWatcher.run: creating pathsToWatchArrayRef", ())
       val pathsToWatchArrayRef = CFArrayCreate(pathsToWatchCfStrings.map(_.getPointer)).getOrElse(
         throw new IllegalStateException(s"CFArrayCreate returned null for $srcs")
       )
       try {
-//        logger("FSEventsWatcher.run: creating streamRef", ())
         val streamRef = FSEventStreamCreate(
           callback,
           pathsToWatchArrayRef,
@@ -117,36 +94,20 @@ class FSEventsWatcher(
               0x00000002
         )
         try {
-          if (useRunLoop) {
-            val current = CFRunLoopGetCurrent()
-            currentRunLoop = Some(current)
-            val runLoopMode = kCFRunLoopDefaultMode
-            FSEventStreamScheduleWithRunLoop(streamRef, current, runLoopMode)
+          val queue = dispatch_queue_create("os.watch.macos.FSEventsWatcher")
+          try {
+            FSEventStreamSetDispatchQueue(streamRef, queue)
             try {
               startFsEventStream(streamRef) {
-                CFRunLoopRun()
+                // We don't really need to have this thread blocked, as macOS has its own dispatch threads,
+                // but because Linux/Windows watchers are blocking, we do the same here in the sake of
+                // simplicity.
+                signal.synchronized(signal.wait())
               }
             }
-            finally {
-              FSEventStreamUnscheduleFromRunLoop(streamRef, current, runLoopMode)
-              FSEventStreamInvalidate(streamRef)
-            }
+            finally FSEventStreamInvalidate(streamRef)
           }
-          else {
-            //          logger("FSEventsWatcher.run: creating queue", ())
-            val queue = dispatch_queue_create("os.watch.macos.FSEventsWatcher")
-            try {
-              //            logger("FSEventsWatcher.run: setting queue", ())
-              FSEventStreamSetDispatchQueue(streamRef, queue)
-              try {
-                startFsEventStream(streamRef) {
-                  waitUntilWeAreToldToStopViaSignal()
-                }
-              }
-              finally FSEventStreamInvalidate(streamRef)
-            }
-            finally dispatch_release(queue)
-          }
+          finally dispatch_release(queue)
         }
         finally FSEventStreamRelease(streamRef)
       }
@@ -155,33 +116,9 @@ class FSEventsWatcher(
     finally pathsToWatchCfStrings.foreach(CFRelease)
   }
 
+  /** This is thread-safe and idempotent. */
   def close(): Unit = {
-    assert(!closed, "Already closed")
-
-    if (useRunLoop) {
-      currentRunLoop match {
-        case Some(runLoop) =>
-          CFRunLoopStop(runLoop)
-          currentRunLoop = None
-        case None =>
-          throw new IllegalStateException("cannot close FSEventsWatcher: currentRunLoop was None")
-      }
-    }
-    else {
-      //    logger("FSEventsWatcher.close: obtaining the lock", ())
-      signal.synchronized {
-        //      logger("FSEventsWatcher.close: notifying the signal", ())
-        signal.notify()
-      }
-      //    logger("FSEventsWatcher.close: notified", ())
-    }
-
+    signal.synchronized(signal.notify())
     closed = true
   }
-}
-object FSEventsWatcher {
-  // Never collected from memory.
-  private lazy val kCFRunLoopDefaultMode: CFStringRef = CFStringRef("kCFRunLoopDefaultMode").getOrElse(
-    throw new IllegalStateException("\"kCFRunLoopDefaultMode\" string could not be created")
-  )
 }
